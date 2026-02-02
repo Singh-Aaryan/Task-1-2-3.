670ae99f5c0a5a6c75b3f4b7ad648c803934c972
Aryan Singh <aryan1005singh@gmail.com>
2026-02-02 13:57:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//Task-1
+const profile = document.getElementById("profile");
+profile.style.backgroundColor="cyan";
+profile.style.padding="15px";
+profile.style.textAlign="center";
+
+//Task-2
+const highlight = document.getElementsByClassName
+("important");
+console.log(highlight.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
